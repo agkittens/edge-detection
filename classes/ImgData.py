6c5ae89b8ab9c4b8bbda203ed3b1e3386a5aaa69
Agnieszka <106694089+agkittens@users.noreply.github.com>
2023-03-18 20:44:07 +0100

    Update ImgData.py

--- classes/ImgData.py
+++ classes/ImgData.py
@@ -1,39 +1,38 @@
 from SobelsMasks import Sobel
 from PIL import Image
+import itertools
 
 class Img:
     def __init__(self, image, imageCopy):
-        self.width = image.width
-        self.height = image.height
+        self.width = image.width - 4
+        self.height = image.height - 4
         self.pixelsValues = imageCopy.load()
         self.pixels = image.load()
 
     def masking(self):
-        for pixelH in range(self.height-4):
-            for pixelW in range(self.width-4):
-                for maskNum in range(4):
-                    rgbColor = [[0]*3]*4
-                    r,g,b = 0, 0, 0
+        for pixelH, pixelW in itertools.product(range(self.height), range(self.width)):
+            for maskNum in range(4):
+                rgbColor = [[0]*3]*4
+                r,g,b = 0, 0, 0
 
-                    for colorNum in range(3):
-                        for y in range(3):
-                            rgbColor[maskNum][colorNum] += (self.pixelsValues[pixelW, pixelH+y][colorNum]*Sobel.masks[maskNum][3*y]+self.pixelsValues[pixelW+1, pixelH+y][colorNum]*Sobel.masks[maskNum][1+3*y]+self.pixelsValues[pixelW+2,pixelH+y][colorNum]*Sobel.masks[maskNum][2+3*y])
+                for colorNum in range(3):
+                    for y in range(9):
+                        rgbColor[maskNum][colorNum] += (self.pixelsValues[pixelW + y%3, pixelH+y//3][colorNum]*Sobel.masks[maskNum][y])
 
-                        if (rgbColor[maskNum][colorNum]<0): rgbColor[maskNum][colorNum]=abs(rgbColor[maskNum][colorNum])
-                        if (rgbColor[maskNum][colorNum]>255): rgbColor[maskNum][colorNum]=255
+                    if (rgbColor[maskNum][colorNum]<0): rgbColor[maskNum][colorNum]=abs(rgbColor[maskNum][colorNum])
+                    if (rgbColor[maskNum][colorNum]>255): rgbColor[maskNum][colorNum]=255
 
-                    r += rgbColor[maskNum][0]
-                    g += rgbColor[maskNum][1]
-                    b += rgbColor[maskNum][2]
+                r += rgbColor[maskNum][0]
+                g += rgbColor[maskNum][1]
+                b += rgbColor[maskNum][2]
 
-                self.pixels[pixelW+1, pixelH+1] = (r, g, b)
+            self.pixels[pixelW+1, pixelH+1] = (r, g, b)
 
     def loadingPixels(self,image):
         imageTransformed = Image.new(mode = 'RGB', size = (self.width, self.height))
 
-        for pixelH in range(self.height-4):
-            for pixelW in range(self.width-4):
-                imageTransformed.putpixel((pixelW+1, pixelH+1),self.pixels[pixelW+1, pixelH+1])
+        for pixelH, pixelW in itertools.product(range(self.height-4), range(self.width-4)):
+            imageTransformed.putpixel((pixelW+1, pixelH+1),self.pixels[pixelW+1, pixelH+1])
 
         imageTransformed.save('sobel.png')
         imageTransformed.close()
